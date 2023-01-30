052a10a69bc038169aa4cd05c46ebb580a7f528c
cloudiosx <cloudiosx@gmail.com>
2023-01-30 07:27:09 -0500

    Day 1 & Day 2

---
+++ Day 1 - variables, simple data types, and string interpolation.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+// Variables
+
+var greeting = "Hello, playground"
+greeting = "Goodbye"
+
+// Strings and integers
+
+var age = 38
+var population = 8_000_000
+
+// Multi-line strings
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// String interpolation
+
+var score = 85
+var str = "Your score was \(score)"
+var results = "The test results are here: \(str)"
+
+// Type annotations
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+// Simple types: Summary
+
+let double: Double = 100_000
+// let integer: Int = 100_000.0 error: cannot convert value of type 'Double' to specified type 'Int'
+
---
+++ Day 2 - arrays, dictionaries, sets, and enums.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// Arrays
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+beatles[1] // "Paul McCartney"
+
+// Sets
+
+let colors = Set(["red", "green", "blue"])
+let colors2 = Set(["red", "green", "blue", "red", "blue"])
+
+// Tuples
+
+var name = (first: "Taylor", last: "Swift", third: "hi")
+var name2 = ("Taylor", "Swift")
+
+name.0
+name.last
+name.third
